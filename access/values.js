d182cac0f0cee91c9c29c34624b67a5e52db0f94
KARNELkoder <106372780+KARNELkoder@users.noreply.github.com>
2022-09-03 15:52:32 +0300

    Update values.js

--- access/values.js
+++ access/values.js
@@ -3,8 +3,8 @@ var isRightsEnabled = true; // Права
 var isCovidCertificateEnabled = true; // COVID-сертификат
 
 // Общее
-var birthdate = "15.07.2004"; // Дата рождения
-var fullname = "Яценко Дмитро Олександрович" // ФИО
+var birthdate = "20.08.2003"; // Дата рождения
+var fullname = "Пучковский Джавид Петрович" // ФИО
 var name = "Имя" // Имя
 
 // Паспорт
